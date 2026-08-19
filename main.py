3c8170cef2abe20fe2209523a0a6cd33940213fd
mattbocc <matthew.boccalon@live.ca>
2026-08-19 16:26:25 -0400

    feat: created and added dataset router to main app routing

--- main.py
+++ main.py
@@ -2,10 +2,13 @@ import os
 from fastapi import FastAPI
 from fastapi.responses import RedirectResponse
 from dotenv import load_dotenv
-
 load_dotenv(override=True)
+from routes.dataset import router as dataset_router
+
 
 app = FastAPI(title="Soft Tissue Sarcoma Portal API", openapi_url="/api/v1/openapi.json", version=f"v{os.getenv('VERSION')}")
+
+app.include_router(dataset_router)
 
 @app.get("/", include_in_schema=False)
 async def root():
