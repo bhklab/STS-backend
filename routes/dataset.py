728983af4f83e91fbeb0c4826cbf4f7c620ce134
mattbocc <matthew.boccalon@live.ca>
2026-08-19 16:28:38 -0400

    feat: added datset routes for the following: getting all datasets + metadata, getting a singular dataset + metadata, getting the data layers that exist for a single dataset, getting the genes that exist for a single dataset

---
+++ routes/dataset.py
@@ -0,0 +1,135 @@
+from sqlalchemy import column
+from models.pre_clinical_tables import PreClinicalTreatmentResponse
+import os
+from pydantic import BaseModel, Field
+from typing import List
+from urllib.parse import quote_plus
+from fastapi import APIRouter, HTTPException, Depends, Query
+from sqlalchemy import create_engine, select, or_
+import pandas as pd
+from database_session import get_db_session
+from models.pre_clinical_tables import PreClinicalDataset, PreClinicalSample, PreClinicalRnaSeq, PreClinicalMutation, PreClinicalMicroarray, PreClinicalCopyNumberVariation, PreClinicalTreatmentResponse
+
+router = APIRouter(prefix="/datasets")
+
+pre_clinical_layers = {
+    "RNA-seq": PreClinicalRnaSeq,
+    "Copy Number Variation": PreClinicalCopyNumberVariation,
+    "Microarray": PreClinicalMicroarray,
+    
+    # "Treatment Response": PreClinicalTreatmentResponse
+}
+    
+# Get all clinical and preclinical datasets
+@router.get(
+    "/all",
+    summary="Get all clinical and preclinical datasets",
+)
+async def get_all_datasets(
+    session=Depends(get_db_session),
+):
+    rows = session.query(PreClinicalDataset).all()
+    return rows
+
+# Get a single clinical or preclinical dataset
+@router.get(
+    "/one",
+    summary="Get a single clinical or preclinical dataset",
+)
+async def get_single_dataset(
+    dataset_id: str = Query(
+        description="The id of the dataset we want to retrieve metadata for",
+        example="1"
+    ),
+    session=Depends(get_db_session),
+):
+    dataset = session.query(PreClinicalDataset).filter(PreClinicalDataset.id == dataset_id).first()
+    return dataset
+
+# Get a single clinical or preclinical dataset statistics
+@router.get(
+    "/one/statistics",
+    summary="Get a single clinical or preclinical dataset",
+)
+async def get_single_dataset_statistics(
+    dataset_id: int = Query(
+        description="The id of the dataset we want to retrieve metadata for",
+        example=1
+    ),
+    session=Depends(get_db_session),
+):
+    dataset = session.query(PreClinicalDataset).filter(PreClinicalDataset.id == dataset_id).first()
+    return dataset
+
+
+# Get all data layers the dataset has data for
+@router.get(
+    "/data-layers",
+    summary="Get all data layers available for a given clinical or preclinical dataset",
+)
+async def get_all_data_layers(
+    dataset_id: int = Query(
+        description="The dataset for which we want to get data layers for.",
+        example=1
+    ),
+    session=Depends(get_db_session),
+):
+    available_layers = []
+
+    if get_clinical_status(dataset_id, session):
+        return []
+    else:
+        # get the data layers available for the dataset by mapping sample_ids to layers
+        for data_layer_name, data_layer_model in pre_clinical_layers.items():
+            row = (
+                session.query(data_layer_model.id)
+                .join(PreClinicalSample, data_layer_model.sample_id == PreClinicalSample.sampleid)
+                .filter(PreClinicalSample.dataset_id == dataset_id)
+                .first()
+            )
+            if row:
+                available_layers.append(data_layer_name)
+    
+    return available_layers
+
+
+# Get all genes from a given clinical or preclinical dataset + data layer
+@router.get(
+    "/genes",
+    summary="Get all genes from a given clinical or preclinical dataset",
+)
+async def get_all_genes(
+    dataset_id: int = Query(
+        description="The id of the dataset we want to retrieve genes for",
+        example=1
+    ),
+    molecular_profile: str = Query(
+        description="select all genes for the data layer",
+        example="RNA-seq"
+    ),
+    session=Depends(get_db_session),
+):
+
+    if get_clinical_status(dataset_id, session):
+        return
+    else:
+        rows = (
+                session.query(pre_clinical_layers[molecular_profile].id)
+                .join(PreClinicalSample, pre_clinical_layers[molecular_profile].sample_id == PreClinicalSample.sampleid)
+                .filter(PreClinicalSample.dataset_id == dataset_id)
+                .all()
+            )
+    
+    return rows
+
+def get_clinical_status (dataset_id, session = Depends(get_db_session)):
+
+    try: 
+        # deduce if dataset is clinical or pre clinical
+        clinical = session.query(PreClinicalDataset.clinical).filter(PreClinicalDataset.id == dataset_id).first()
+    except Exception as e:
+        raise HTTPException(status_code=404, detail="Dataset not found")
+
+    return clinical
+
+
